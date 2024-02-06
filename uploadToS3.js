04f41e996f66627ae1c1de2b6305e7d3a4e7fd75
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 17:52:06 +0100

    Update uploadToS3.js

--- uploadToS3.js
+++ uploadToS3.js
@@ -1,23 +1,23 @@
-const fs = require('fs')
-const axios = require('axios')
+const fs = require('fs');
+const axios = require('axios');
 
-const folderPath = '/dist';  // Change this to the path of your folder
-const presignedUrl = process.argv[4]
+const folderPath = '/path/to/your/folder';  // Change this to the path of your folder
+const presignedUrl = process.argv[4];
 
-const files = fs.readdirSync(folderPath)
+const files = fs.readdirSync(folderPath);
 
 files.forEach(async (file) => {
-  const filePath = `${folderPath}/${file}`
-  const fileContent = fs.readFileSync(filePath)
+  const filePath = `${folderPath}/${file}`;
+  const fileContent = fs.readFileSync(filePath);
 
   try {
     await axios.put(presignedUrl, fileContent, {
       headers: {
         'Content-Type': 'application/octet-stream',
       },
-    })
-    console.log(`Uploaded ${file} to S3`)
+    });
+    console.log(`Uploaded ${file} to S3`);
   } catch (error) {
-    console.error(`Error uploading ${file} to S3:`, error.message)
+    console.error(`Error uploading ${file} to S3:`, error.message);
   }
-})
+});
