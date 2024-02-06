f05f2434f48c867c25b3557844e7087b918a820e
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 17:51:16 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -1,11 +1,16 @@
-const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
-const bucketName = process.argv[2]
-const objectKey = process.argv[3]
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+const bucketName = process.argv[2];
+const objectKey = process.argv[3];
+
 const params = {
-  Bucket: bucketName
-  Key: objectKey
-  Expires: 3600
-}
-const presignedUrl = s3.getSignedUrl('putObject', params)
-console.log('Pre-signed URL:', presignedUrl)
+  Bucket: bucketName,
+  Key: objectKey,
+  Expires: 3600,
+};
+
+const presignedUrl = s3.getSignedUrl('putObject', params);
+
+console.log('Pre-signed URL:', presignedUrl);
