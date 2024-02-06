7e501949773e153f0db0e5b0b7c610c9a746dfb8
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 23:41:55 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -1,11 +1,18 @@
-const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
+
 const awsRegion = process.env.AWS_REGION || 'us-east-1'
 const awsAccessId = process.env.AWS_ACCESS_ID
 const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-const s3Client = new S3Client({region: ${awsRegion},credentials:{ accessKeyId:${awsAccessId},secretAccessKey:${awsSecretAccessKey}}})
 
-// import { PutObjectCommand } from '@aws-sdk/client-s3'
+const s3Client = new S3Client({
+  region: awsRegion,
+  credentials: {
+    accessKeyId: awsAccessId,
+    secretAccessKey: awsSecretAccessKey
+  }
+})
+
 // Configure the S3 client
 const bucketName = process.argv[2]
 const objectKey = process.argv[3]
@@ -16,17 +23,19 @@ if (!bucketName || !objectKey) {
   console.error('Please provide both bucketName and objectKey as command line arguments.')
   process.exit(1)
 }
+
 // Generate a presigned URL
+(async () => {
   // Create a GetObjectCommand
   const getObjectCommand = new GetObjectCommand({
     Bucket: bucketName,
     Key: objectKey
   })
-(async () => {
-  const presignedUrl = await getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
-  console.log('Presigned URL:' presignedUrl)
-})()
 
-// Call the function to generate and use the presigned URL
-//const presignedUrl = getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
-//console.log('Presigned URL:', presignedUrl)
+  try {
+    const presignedUrl = await getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
+    console.log('Presigned URL:', presignedUrl)
+  } catch (error) {
+    console.error('Error generating presigned URL:', error)
+  }
+})()
