f6fd5e1dd791e4f48d080654eb41574d27ae74d0
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 21:46:51 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -25,5 +25,31 @@ if (!bucketName || !objectKey) {
 
 
 // Call the function to generate and use the presigned URL
-const presignedUrl = getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
-console.log('Presigned URL:', presignedUrl)
+//const presignedUrl = getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
+//console.log('Presigned URL:', presignedUrl)
+
+++++++++++++++++
+async function generatePresignedUrl() {
+  // Configure the S3 client
+ 
+  // Specify the S3 bucket and object key
+// Generate a presigned URL
+  const s3Client = new S3Client({ region: region })
+  // Create a GetObjectCommand
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: objectKey
+  })
+
+  return new Promise((resolve, reject) => {
+    // Simulate an asynchronous operation with setTimeout
+    setTimeout(() => {
+      // Generate a presigned URL
+      const presignedUrl = getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
+      // Set expiresIn to the desired expiration time in seconds
+
+      // Resolve the promise with the presigned URL
+      resolve(presignedUrl)
+    }, 10000)
+  })
+}
