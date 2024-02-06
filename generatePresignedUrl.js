badde86cfa3f17692621c0a67e44c179d82abf95
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 20:15:18 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -1,10 +1,8 @@
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { PutObjectCommand} from '@aws-sdk/client-s3'
+// import { PutObjectCommand } from '@aws-sdk/client-s3'
 // Configure the S3 client
-const s3Client = new S3Client({ region: region })
-// Printing process.argv property value 
-console.log(process.argv)
+
 
 const bucketName = process.argv[2]
 const objectKey = process.argv[3]
@@ -16,7 +14,9 @@ if (!bucketName || !objectKey) {
   console.error('Please provide both bucketName and objectKey as command line arguments.')
   process.exit(1) // Exit with an error code
 }
-
+const s3Client = new S3Client({ region: region })
+// Printing process.argv property value 
+console.log(process.argv)
 // Create a GetObjectCommand
 const getObjectCommand = new GetObjectCommand({
   Bucket: bucketName,
