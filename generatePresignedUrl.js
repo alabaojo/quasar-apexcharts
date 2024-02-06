f1ed54c4dc13f048f3b850b2b893dbea5b339712
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 22:16:10 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -28,6 +28,6 @@ function delay(ms) {
 
 
 // Call the function to generate and use the presigned URL
-const presignedUrl = await getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
+const presignedUrl = getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 })
   await delay(5000);
 console.log('Presigned URL:', presignedUrl)
