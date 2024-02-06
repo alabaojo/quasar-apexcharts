0f52cb8d8718c7984ec152da44a10d86322de8ff
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-06 18:32:25 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -5,6 +5,12 @@ const s3 = new AWS.S3()
 const bucketName = process.argv[2]
 const objectKey = process.argv[3]
 
+if (!bucketName || !objectKey) {
+  console.log(bucketName)
+  console.log(objectKey)
+  console.error('Please provide both bucketName and objectKey as command line arguments.');
+  process.exit(1); // Exit with an error code
+}
 const params = {
   Bucket: bucketName,
   Key: objectKey,
