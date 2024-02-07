1ca0ce33ba077a2c254162caef2e8cc6613e6fff
Alaba Ojo <alabaojo@users.noreply.github.com>
2024-02-07 14:09:53 +0100

    Update generatePresignedUrl.js

--- generatePresignedUrl.js
+++ generatePresignedUrl.js
@@ -71,7 +71,7 @@ async function uploadZipToS3() {
   try {
     const presignedUrl = await getSignedUrl(s3Client, putObjectCommand, { expiresIn: 3600 });
     console.log('Presigned URL:', presignedUrl);
-    return presignedUrl;
+    process.env.S3_PRESIGNED_URL = presignedUrl;
   } catch (error) {
     console.error('Error generating presigned URL:', error);
   }
